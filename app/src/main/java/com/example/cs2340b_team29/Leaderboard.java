2a2216962ef308e90ec907b91c524d5b9f0889c3
roserwen <roserwen@gmail.com>
2023-10-11 22:51:03 -0400

    add leaderboard tests

--- app/src/main/java/com/example/cs2340b_team29/Leaderboard.java
+++ app/src/main/java/com/example/cs2340b_team29/Leaderboard.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class Leaderboard {
     private volatile static Leaderboard uniqueInstance;
     private ArrayList<LeaderboardEntry> attempts;
-    private Leaderboard(){}
+    private Leaderboard(){
+        attempts = new ArrayList<>();
+    }
     public static Leaderboard getLeaderboard(){
         if (uniqueInstance == null) {
             synchronized (Leaderboard.class) {
