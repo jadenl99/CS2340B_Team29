90a5a162cf6d24f9c1ca6a4d67efabf58c7f52aa
jadenl99 <100039013+jadenl99@users.noreply.github.com>
2023-10-30 14:14:26 -0400

    factory pattern for enemies

--- app/src/main/java/com/example/cs2340b_team29/model/Enemy.java
+++ app/src/main/java/com/example/cs2340b_team29/model/Enemy.java
@@ -3,5 +3,5 @@ package com.example.cs2340b_team29.model;
 import com.example.cs2340b_team29.collision.Collidable;
 
 public interface Enemy extends Collidable {
-
+    // TODO add common actions of enemies here like attack()
 }
