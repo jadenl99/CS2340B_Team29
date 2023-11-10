bc6183da65be4f2dda7af662680578bc5bcdb6fc
roserwen <roserwen@gmail.com>
2023-11-10 15:16:03 -0500

    update parameters of attack method in Enemy class

--- app/src/main/java/com/example/cs2340b_team29/model/Enemy.java
+++ app/src/main/java/com/example/cs2340b_team29/model/Enemy.java
@@ -10,6 +10,8 @@ public abstract class Enemy extends Entity {
     }
 
     public abstract int getEnemyID();
+    public abstract void attack(Player player, int difficulty);
+
     public abstract void attack();
     //each enemy implements attack differently
 
--- app/src/main/java/com/example/cs2340b_team29/model/Ninja.java
+++ app/src/main/java/com/example/cs2340b_team29/model/Ninja.java
@@ -1,12 +1,5 @@
 package com.example.cs2340b_team29.model;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.content.Context;
-
-import com.example.cs2340b_team29.R;
-
 public class Ninja extends Enemy {
 
     private int enemyID = 1;
