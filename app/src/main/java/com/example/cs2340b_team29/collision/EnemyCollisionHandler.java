38580172dca1de6430a7c43106ea9fcc5713c5a0
roserwen <roserwen@gmail.com>
2023-11-12 17:04:16 -0500

    stops player from being able to overlap enemy when player moves

--- app/src/main/java/com/example/cs2340b_team29/collision/EnemyCollisionHandler.java
+++ app/src/main/java/com/example/cs2340b_team29/collision/EnemyCollisionHandler.java
@@ -12,6 +12,19 @@ import com.example.cs2340b_team29.viewmodel.MoveUp;
 public class EnemyCollisionHandler implements CollisionObserver{
     @Override
     public void onCollision(Collidable player1, Collidable enemy1, MoveStrategy moveStrategy) {
+        //don't let player and enemy go into same box
+        if (player1 instanceof Player && enemy1 instanceof Enemy) {
+            Player player = (Player) player1;
+            if (moveStrategy instanceof MoveDown) {
+                player.setY(player.getY() - 1);
+            } else if (moveStrategy instanceof MoveUp) {
+                player.setY(player.getY() + 1);
+            } else if (moveStrategy instanceof MoveRight) {
+                player.setX(player.getX() - 1);
+            } else {
+                player.setX(player.getX() + 1);
+            }
+        }
             Player player = (Player) player1;
             int difficulty = MapData.getMapData().getDifficulty();
             if (enemy1 instanceof Enemy) {
