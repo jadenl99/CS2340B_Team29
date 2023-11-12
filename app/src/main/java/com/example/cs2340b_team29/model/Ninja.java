3ac2e1a12d1f9c14437465e9e251c2cffd7088bd
gracem2023 <gmoussouri@gmail.com>
2023-11-12 16:21:37 -0500

    fixes enemy movement

--- app/src/main/java/com/example/cs2340b_team29/model/Ninja.java
+++ app/src/main/java/com/example/cs2340b_team29/model/Ninja.java
@@ -1,15 +1,36 @@
 package com.example.cs2340b_team29.model;
 
+import com.example.cs2340b_team29.collision.Collidable;
+import com.example.cs2340b_team29.collision.CollisionObserver;
+import com.example.cs2340b_team29.viewmodel.MoveStrategy;
+
+import java.util.ArrayList;
+
 public class Ninja extends Enemy {
 
     private int enemyID = 1;
+    private ArrayList<CollisionObserver> observers;
     public Ninja() {
         super(0, 0);
+        observers = new ArrayList<>();
     }
 
 
     public int getEnemyID() {
         return enemyID;
+    }
+
+    public void subscribe(CollisionObserver observer) {
+        observers.add(observer);
+    }
+    public void unsubscribe(CollisionObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyCollision(Collidable entity, MoveStrategy moveStrategy) {
+        for (CollisionObserver observer : observers) {
+            observer.onCollision(this, entity, moveStrategy);
+        }
     }
 
 
