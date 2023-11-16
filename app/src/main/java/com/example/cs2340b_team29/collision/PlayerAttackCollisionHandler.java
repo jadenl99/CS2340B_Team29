a57e2c11a50ccf60529f5ff62c90b3efc027194b
gracem2023 <gmoussouri@gmail.com>
2023-11-16 14:14:21 -0500

    adds player attack collision handler

--- app/src/main/java/com/example/cs2340b_team29/collision/PlayerAttackCollisionHandler.java
+++ app/src/main/java/com/example/cs2340b_team29/collision/PlayerAttackCollisionHandler.java
@@ -8,9 +8,15 @@ import com.example.cs2340b_team29.viewmodel.MoveOffScreen;
 import com.example.cs2340b_team29.viewmodel.MoveRight;
 import com.example.cs2340b_team29.viewmodel.MoveStrategy;
 import com.example.cs2340b_team29.viewmodel.MoveUp;
+import com.example.cs2340b_team29.model.Knife;
+import com.example.cs2340b_team29.model.Player;
+import com.example.cs2340b_team29.model.Weapon;
+import com.example.cs2340b_team29.viewmodel.MoveOffScreen;
+import com.example.cs2340b_team29.viewmodel.MoveStrategy;
 
 public class PlayerAttackCollisionHandler implements Collidable {
-    public void onCollision(Collidable e1, Collidable e2, MoveStrategy moveStrategy) {
+
+    public void onCollision(Collidable e1, Collidable e2, MoveStrategy strategy) {
         if (e1 instanceof Player && e2 instanceof Enemy) {
             MoveStrategy offScreen = new MoveOffScreen();
             Player player = (Player) e1;
