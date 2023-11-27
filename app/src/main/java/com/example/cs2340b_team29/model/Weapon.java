d2c4da615dc224ab4c14de9cd4fbc7c9183eb33d
jadenl99 <100039013+jadenl99@users.noreply.github.com>
2023-11-27 13:05:21 -0500

    fix classing in weapons

--- app/src/main/java/com/example/cs2340b_team29/model/Weapon.java
+++ app/src/main/java/com/example/cs2340b_team29/model/Weapon.java
@@ -7,9 +7,15 @@ import com.example.cs2340b_team29.collision.CollisionObserver;
 import com.example.cs2340b_team29.viewmodel.MoveStrategy;
 
 public abstract class Weapon implements Collidable {
+    protected int x;
+    protected int y;
     Bitmap bitmap;
     int weaponId;
     boolean isVisible;
+    public Weapon(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
     public abstract int getWeaponId();
 
     public abstract void subscribe(CollisionObserver observer);
@@ -28,5 +34,21 @@ public abstract class Weapon implements Collidable {
 
     public abstract Bitmap getBitmap();
     public abstract void setBitmap(Bitmap bitmap);
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
 
 }
