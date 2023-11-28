43f1238ad311eb2f0966009f03177dc31df4e4d6
gracem2023 <gmoussouri@gmail.com>
2023-11-28 17:28:03 -0500

    attack implementation works for all levels

--- app/src/main/java/com/example/cs2340b_team29/collision/PlayerAttackCollisionHandler.java
+++ app/src/main/java/com/example/cs2340b_team29/collision/PlayerAttackCollisionHandler.java
@@ -19,7 +19,7 @@ public class PlayerAttackCollisionHandler implements CollisionObserver {
     public void onCollision(Collidable e1, Collidable e2, MoveStrategy strategy) {
         if (e1 instanceof Player && e2 instanceof Enemy) {
             Player player = (Player) e1;
-            if (player.getHasSword() || player.getHasKnife() && player.getIsAttacking()) {
+            if (player.getHasSword() || player.getHasKnife()) {
                 MoveStrategy offScreen = new MoveOffScreen();
                 Enemy enemy = (Enemy) e2;
                 enemy.setMoveStrategy(offScreen);
